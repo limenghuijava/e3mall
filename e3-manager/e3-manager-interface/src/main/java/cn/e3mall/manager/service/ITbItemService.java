698a93ea4182fa95d13f41acb795069e839dd2aa
limenghuijava <@20111102lmh>
2018-03-05 13:08:45 +0800

    ssm框架一些错误的修改，并成功测试框架的搭建成功

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/manager/service/ITbItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/manager/service/ITbItemService.java
@@ -2,8 +2,6 @@ package cn.e3mall.manager.service;
 
 import cn.e3mall.manager.pojo.TbItem;
 
-
 public interface ITbItemService {
-
     TbItem findTbItemById(Long itemId);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/manager/impl/TbItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/manager/impl/TbItemServiceImpl.java
@@ -1,14 +1,15 @@
-package cn.e3mall.manager.service;
+package cn.e3mall.manager.impl;
 
 import cn.e3mall.manager.mapper.ITbItemMapper;
 import cn.e3mall.manager.pojo.TbItem;
+import cn.e3mall.manager.service.ITbItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("tbItemService")
-public class TbItemServiceImpl  implements ITbItemService{
+@Service
+public class TbItemServiceImpl  implements ITbItemService {
 
     @Autowired
     private ITbItemMapper tbItemMapper;
